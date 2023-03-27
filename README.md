3891c6f7bb3272229922cf75edd5470c5200ad91
Ambria Johnson <1223ambria@gmail.com>
2023-03-26 21:09:13 -0400

    Read.Me was added

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Professional-READ.me
+# Professional READ.Me
+
+This project allows developers to create a professional READ.Me file using node.js. This allows developers to spend more time developing their projects than curating a READ.Me file.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+Clone the guthub repo. In the terminal use the command node node.js in order to intialize the questions for the READ.me.
+
+## Usage
+
+
+
+## Contributing
+
+N/A
+
+## Tests
+
+N/A
+
+## Questions
+
+If you have any questions, please feel free to contact me:
+
+- GitHub: https://github.com/Ambria1223
+- Email: 1223ambria@gmail.com
--- node.js
+++ node.js
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-// Prompt the user for input
+
 rl.question("Enter the project name: ", (project_name) => {
   rl.question("Enter a short description of the project: ", (project_description) => {
     rl.question("Enter the installation instructions: ", (installation_instructions) => {
@@ -15,10 +15,10 @@ rl.question("Enter the project name: ", (project_name) => {
           rl.question("Enter the test instructions: ", (test_instructions) => {
             rl.question("Enter your GitHub username: ", (github_username) => {
               rl.question("Enter your email address: ", (email_address) => {
-                // Generate the README.md file content
+            
                 const readmeContent = `# ${project_name}\n\n${project_description}\n\n## Table of Contents\n\n- [Installation](#installation)\n- [Usage](#usage)\n- [Contributing](#contributing)\n- [Tests](#tests)\n- [Questions](#questions)\n\n## Installation\n\n${installation_instructions}\n\n## Usage\n\n${usage_information}\n\n## Contributing\n\n${contribution_guidelines}\n\n## Tests\n\n${test_instructions}\n\n## Questions\n\nIf you have any questions, please feel free to contact me:\n\n- GitHub: https://github.com/${github_username}\n- Email: ${email_address}`;
                 
-                // Write the README.md file to disk
+               
                 fs.writeFile("README.md", readmeContent, (err) => {
                   if (err) throw err;
                   console.log("README.md file generated successfully!");
